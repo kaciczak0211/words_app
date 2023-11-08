aa64806d9f2cd9ac4b03c9ce7ef9692c0ac4d1bf
kaciczak0211 <kaciczak0211@gmail.com>
2023-11-08 23:04:51 +0000

    fetch

--- min.js
+++ min.js
@@ -1,45 +1,20 @@
-let engWord = document.getElementById("eng-word");
-let plWord = document.getElementById("pl-word");
-let checkBtn = document.getElementById("check-btn");
-let nextWord = document.getElementById("next");
-let correct = document.getElementById("correct");
-let start = document.getElementById("start");
-let mainContainer = document.getElementById("main-con");
+async function findWord(){
+    const word = document.getElementById("word").value
 
-
-//Arrays with words, database
-
-let engArray = ["music", "tree", "ball", "lamp"];
-let plArray = ["muzyka", "drzewo", "piłka", "lampa"];
-let i = 0;
-
-function loadContent(){
-    mainContainer.style.display = "block"
-    start.style.display = "none";
-    engWord.textContent = engArray[i]
-    plWord.textContent = plArray[i]
-    i++
-    if(i > engArray.length){
-        engWord.textContent = "end of the game, you konw all words!"
-        plWord.textContent = "";
+    if(!word){
+        alert("write a word")
     }
-    plWord.style.color = "white"
-    correct.addEventListener("click", ()=>{
-        engWord.style.color = "green"
-        plWord.style.color = "green"
-        
-    })
+
+    const findWord = new URL(`https://api.dictionaryapi.dev/api/v2/entries/en/${ word }`)
+    console.log(findWord);
+
+    findWord.searchParams.set("token", "YOUR_TOKEN_HERE")
+
+    const response = await fetch(findWord)
+
+    console.log(response);
+
+    const data = await response.json();
+
+    console.log(data.word)
 }
-
-start.addEventListener("click", loadContent);
-nextWord.addEventListener("click",loadContent)
-// nextWord.addEventListener("click",hideBtn)
-
-// function hideBtn(){
-//     start.style.display = "none";
-// }
-
-checkBtn.addEventListener("click", ()=>{
-    plWord.style.color = "black"
-})
-
