83c2c850eeec88e80f721aa0cb6a05b9e65cf3b2
kaciczak0211 <kaciczak0211@gmail.com>
2023-11-14 07:17:05 +0000

    css_update

--- min.js
+++ min.js
@@ -16,9 +16,9 @@ btn.addEventListener("click", function(){
         result.innerHTML = `        
         <h3 id="title">${wordValue}</h3>
         <h3 id="pos">${data[0].meanings[0].partOfSpeech}</h3>
+        <button onclick="playSound()"><i class="fa-solid fa-volume-high" style="color: #000000;"></i></button>
         <h3 id="def">${data[0].meanings[0].definitions[0].definition}</h3>
         <h3 id="word-example">${data[0].meanings[0].definitions[0].example || ""}</h3>
-        <button onclick="playSound()"><i class="fa-solid fa-volume-high" style="color: #000000;"></i></button>
         `
         let audioMp3 = data[0].phonetics[0].audio || data[0].phonetics[1].audio || data[0].phonetics[2].audio || data[0].phonetics[3].audio
         console.log(sound);
@@ -35,3 +35,4 @@ function playSound(){
     sound.play();
     console.log("play")
 }
+
